7eecfecae935fda26ed43ea54cce9d27209bb6d2
PabloRR100 <pabloruizruiz10@gmail.com>
2018-08-28 13:35:03 -0400

    testing oddyssey

--- ResNets/demo_CIFAR10.sh
+++ ResNets/demo_CIFAR10.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+# Confuguration for multiple GPU on a single Machine
+
 #SBATCH -n 1                # Number of cores
 #SBATCH -N 1                # Ensure that all cores are on one machine
 #SBATCH -t 0-00:10          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p serial_requeue   # Partition to submit to
 
 # Testing mode - quickly allocation of resources
+
 #SBATCH --mem=100         
 
 # Uncomment to full power
+
 # #SBATCH --mem=1000          # Memory pool for all cores (see also --mem-per-cpu)
 # #SBATCH --gres=gpu:8        # Activate n GPU (let's say 8)
-
 
 #SBATCH -o myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
@@ -21,11 +24,14 @@ module load Anaconda3/5.0.1-fasrc02     # Load Anaconda
 module load cuda/9.0-fasrc02 cudnn/7.0_cuda9.0-fasrc01
 
 # Activate Environment
+
 source activate pytorch
+
+
 
 # ResNet CIFAR10
 # --------------
 
 # Big Ensemble
-python demo_CIFAR10.py -n ResNet --save True --testing False --comments True --draws False --ensembleSize Big --dataset CIFAR10 
+python demo_CIFAR10.py --name ResNet --save True --testing False --comments True --draws False --ensembleSize Big --dataset CIFAR10 
  
