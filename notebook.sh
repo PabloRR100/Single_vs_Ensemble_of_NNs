17a96c98bcd819e7a181323cb2a4c30f840334e3
PabloRR100 <pabloruizruiz10@gmail.com>
2018-08-30 18:07:05 -0400

    cuda version

--- notebook.sh
+++ notebook.sh
@@ -5,7 +5,7 @@ module load Anaconda3/5.0.1-fasrc02
 
 if [ $1 == 'cuda80' ]
 then
-  module load cuda/8.0.61-fasrc01 cudnn/6.0_cuda8.0-fasrc0
+  module load cuda/8.0.61-fasrc01 cudnn/6.0_cuda8.0-fasrc01
   source activate cuda8
 
 elif [ $1 == 'cuda90' ]
