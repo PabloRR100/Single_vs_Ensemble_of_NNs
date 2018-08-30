9e53dcf7295d0606976e2b3ddc63f10cbc347e4a
PabloRR100 <pabloruizruiz10@gmail.com>
2018-08-30 17:00:29 -0400

    correct env

--- notebook.sh
+++ notebook.sh
@@ -6,7 +6,7 @@ module load Anaconda3/5.0.1-fasrc02
 if [ $1 == 'cuda80' ]
 then
   module load cuda/8.0.61-fasrc01 cudnn/6.0_cuda8.0-fasrc0
-  source activate cuda80
+  source activate cuda8
 
 elif [ $1 == 'cuda90' ]
 then
