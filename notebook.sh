910a6f19f4c3f6c2b6b6b9eb660775787aa24069
PabloRR100 <pabloruizruiz10@gmail.com>
2018-08-30 16:31:25 -0400

    different cuda versions

--- notebook.sh
+++ notebook.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-module load git/2.17.0-fasrc01
 module load Anaconda3/5.0.1-fasrc02
-module load cuda/9.0-fasrc02 cudnn/7.0_cuda9.0-fasrc01
 
-# Activate Environment
-source activate torch37
+if [ $1 == 'cuda80' ]
+then
+  module load cuda/8.0.61-fasrc01 cudnn/6.0_cuda8.0-fasrc0
+  source activate cuda80
 
+elif [ $1 == 'cuda90']
+then
+  module load cuda/9.0-fasrc02 cudnn/7.0_cuda9.0-fasrc01
+  source activate torch37
+
+# Output information
 which python
 python -V
 
 # Launch notebook
 jupyter notebook --port 8899
- 
