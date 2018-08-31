7b2b209e027ccc25d8eddb392c21535a70c7efc7
PabloRR100 <pabloruizruiz10@gmail.com>
2018-08-31 10:23:51 -0400

    lab

--- notebook.sh
+++ notebook.sh
@@ -10,13 +10,14 @@ if [ $2 == 'cuda80' ]
 then
   echo Activating environment cuda8...
   module load cuda/8.0.61-fasrc01 cudnn/6.0_cuda8.0-fasrc01
-  source activate cuda8
+  CONDA="cuda8"
 
 elif [ $2 == 'cuda90' ]
 then
   echo Activating environment torch37...
   module load cuda/9.0-fasrc02 cudnn/7.0_cuda9.0-fasrc01
-  source activate torch37
+  ENV="torch37"
+
 else
   echo Environment not found
   echo Please, rememeber to insert env as parameter
@@ -25,11 +26,12 @@ else
 fi
 
 # Output information
+source activate $CONDA
 which python
 python -V
 
 # Launch notebook
-if [$1 == 'notebook']
+if [ $1 == 'notebook' ]
 then
   echo Loading Jupyer Notebook ...
   jupyter notebook --port 8899
