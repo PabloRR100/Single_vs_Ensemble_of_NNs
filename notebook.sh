9e2023798c18e19e4fee33a4a9d22017873f3971
PabloRR100 <pabloruizruiz10@gmail.com>
2018-08-31 09:39:01 -0400

    lab

--- notebook.sh
+++ notebook.sh
@@ -3,13 +3,13 @@
 module load git/2.17.0-fasrc01
 module load Anaconda3/5.0.1-fasrc02
 
-if [ $1 == 'cuda80' ]
+if [ $2 == 'cuda80' ]
 then
   echo Activating environment cuda8...
   module load cuda/8.0.61-fasrc01 cudnn/6.0_cuda8.0-fasrc01
   source activate cuda8
 
-elif [ $1 == 'cuda90' ]
+elif [ $2 == 'cuda90' ]
 then
   echo Activating environment torch37...
   module load cuda/9.0-fasrc02 cudnn/7.0_cuda9.0-fasrc01
@@ -26,4 +26,10 @@ which python
 python -V
 
 # Launch notebook
-jupyter notebook --port 8899
+if [$1 == 'notebook']
+then
+  echo Loading Jupyer Notebook ...
+  jupyter notebook --port 8899
+else
+  echo Loading Jupyter Lab ...
+  jupyter lab --port 8899
