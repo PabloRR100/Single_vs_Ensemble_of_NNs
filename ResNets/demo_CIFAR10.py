b63cce6e039d6e8b830fbe9376f842a713de1f20
PabloRR100 <pabloruizruiz10@gmail.com>
2018-09-13 18:49:01 -0400

    adjust saving dict

--- ResNets/demo_CIFAR10.py
+++ ResNets/demo_CIFAR10.py
@@ -281,7 +281,7 @@ print('\n\nTRAINING')
 print('--------'); bl
 
 from train import train
-from train_ensemble import train as train_ensemble
+from train_ensemble import train_ensemble
 criterion = nn.CrossEntropyLoss().cuda() if cuda else nn.CrossEntropyLoss()
 
 # Big Single Model
@@ -289,7 +289,7 @@ criterion = nn.CrossEntropyLoss().cuda() if cuda else nn.CrossEntropyLoss()
 optimizers = [optim.SGD(singleModel.parameters(), learning_rate, momentum, weight_decay)]
 
 print('Starting Single Model Training...' )
-params = [dataset, name, [singleModel], optimizers, criterion, device, train_loader,
+params = [dataset, name, singleModel, optimizers, criterion, device, train_loader,
           valid_loader, n_epochs, n_iters, save, paths, save_frequency, testing]
 
 results, timer = train(*params)
