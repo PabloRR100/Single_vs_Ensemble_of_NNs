de66dc33e151de31b1a0bb54039e6fe9c1aee077
PabloRR100 <pabloruizruiz10@gmail.com>
2018-09-17 16:02:15 -0400

    ready to train Train/TestResults Class

--- ResNets/results.py
+++ ResNets/results.py
@@ -1,4 +1,5 @@
 
+from pandas import concat, DataFrame
 from collections import OrderedDict as ordict
 from beautifultable import BeautifulTable as BT
 
@@ -162,3 +163,79 @@ class TestResults():
         
         self.single_accy
         self.ensemble_accy
+        
+
+def aggregateResults(res, eres):
+    
+    # Training Loss Per Iteration
+    iter_train_loss = concat((DataFrame(res.iter_train_loss, columns=['ResNet56']), 
+                                 DataFrame.from_dict(eres.iter_train_loss)), axis=1)
+    
+    # Training Loss Per Epoch
+    epoch_train_loss = concat((DataFrame(res.train_loss, columns=['ResNet56']), 
+                                  DataFrame.from_dict(eres.train_loss)), axis=1)
+    
+    # Training Accuracy Per Iteration
+    iter_train_accy = concat((DataFrame(res.iter_train_accy, columns=['ResNet56']), 
+                                 DataFrame.from_dict(eres.iter_train_accy)), axis=1)
+    
+    # Training Accuracy Per Epoch
+    epoch_train_accy = concat((DataFrame(res.train_accy, columns=['ResNet56']), 
+                                  DataFrame.from_dict(eres.train_accy)), axis=1)
+    
+    # Training Test Error Per Iteration
+    iter_train_testerror = 100 - iter_train_accy.iloc[:,:]
+    
+    # Training Test Error Per Epoch
+    epoch_train_testerror = 100 - iter_train_accy.iloc[:,:]
+    
+    
+    # Validation Loss
+    valid_loss = concat((DataFrame(res.valid_loss, columns=['ResNet56']), 
+                            DataFrame.from_dict(eres.valid_loss)), axis=1)
+    
+    # Validation Accuracy 
+    valid_accy = concat((DataFrame(res.valid_accy, columns=['ResNet56']), 
+                            DataFrame.from_dict(eres.train_accy)), axis=1)
+    
+    # Validation Test Error
+    valid_testerror = 100 - valid_accy.iloc[:,:]
+    
+    
+    # TRAINING DATA
+    train = {'iter': 
+        
+                {'loss': iter_train_loss,
+                 'accy': iter_train_accy,
+                 'test': iter_train_testerror
+                 },
+    
+             'epoch':
+        
+                {'loss': epoch_train_loss,
+                 'accy': epoch_train_accy,
+                 'test': epoch_train_testerror
+                 },
+       }
+    
+    
+    # VALIDATION DATA
+    valid = {'loss': valid_loss,
+             'accy': valid_accy,
+             'test': valid_testerror
+             }
+    
+    
+    
+    # TESTING DATA
+    test = {'single': 'placeholder',
+            'ensemble': 'placeholder'}
+    
+    
+    
+    # DATA DICTIONARY
+    data = {'train': train,
+            'valid': valid,
+            'test': test}
+
+    return data
--- ResNets/results_dashboard.py
+++ ResNets/results_dashboard.py
@@ -1,6 +1,7 @@
 
 import os
 import pickle
+import numpy as np
 import pandas as pd
 import plotly.graph_objs as go
 
@@ -9,153 +10,96 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
+from results import aggregateResults
 
-# Training figures
-with open('Results_Single_Models.pkl', 'rb') as input:
-    res = pickle.load(input)
-
-with open('Results_Ensemble_Models.pkl', 'rb') as input:
-    eres = pickle.load(input)
 
 ## Training figures
-#with open('../Results_Single_Models_Backup.pkl', 'rb') as input:
+#with open('Results_Single_Models.pkl', 'rb') as input:
 #    res = pickle.load(input)
 #
-#with open('../Results_Ensemble_Models_Backup.pkl', 'rb') as input:
+#with open('Results_Ensemble_Models.pkl', 'rb') as input:
 #    eres = pickle.load(input)
 
+# Training figures
+with open('../Results_Single_Models_Backup.pkl', 'rb') as input:
+    res = pickle.load(input)
 
-# Training Loss Per Iteration
-iter_train_loss = pd.concat((pd.DataFrame(res.iter_train_loss, columns=['ResNet56']), 
-                             pd.DataFrame.from_dict(eres.iter_train_loss)), axis=1)
+with open('../Results_Ensemble_Models_Backup.pkl', 'rb') as input:
+    eres = pickle.load(input)
 
-# Training Loss Per Epoch
-epoch_train_loss = pd.concat((pd.DataFrame(res.train_loss, columns=['ResNet56']), 
-                              pd.DataFrame.from_dict(eres.train_loss)), axis=1)
-
-# Training Accuracy Per Iteration
-iter_train_accy = pd.concat((pd.DataFrame(res.iter_train_accy, columns=['ResNet56']), 
-                             pd.DataFrame.from_dict(eres.iter_train_accy)), axis=1)
-
-# Training Accuracy Per Epoch
-epoch_train_accy = pd.concat((pd.DataFrame(res.train_accy, columns=['ResNet56']), 
-                              pd.DataFrame.from_dict(eres.train_accy)), axis=1)
-
-# Training Test Error Per Iteration
-iter_train_testerror = 100 - iter_train_accy.iloc[:,:]
-
-# Training Test Error Per Epoch
-epoch_train_testerror = 100 - iter_train_accy.iloc[:,:]
-
-
-# Validation Loss
-valid_loss = pd.concat((pd.DataFrame(res.valid_loss, columns=['ResNet56']), 
-                        pd.DataFrame.from_dict(eres.valid_loss)), axis=1)
-
-# Validation Accuracy 
-valid_accy = pd.concat((pd.DataFrame(res.valid_accy, columns=['ResNet56']), 
-                        pd.DataFrame.from_dict(eres.train_accy)), axis=1)
-
-# Validation Test Error
-valid_testerror = 100 - valid_accy.iloc[:,:]
-
-
-# TRAINING DATA
-train = {'iter': 
-    
-            {'loss': iter_train_loss,
-             'accy': iter_train_accy,
-             'test': iter_train_testerror
-             },
-
-         'epoch':
-    
-            {'loss': epoch_train_loss,
-             'accy': epoch_train_accy,
-             'test': epoch_train_testerror
-             },
-   }
-
-
-# VALIDATION DATA
-valid = {'loss': valid_loss,
-         'accy': valid_accy,
-         'test': valid_testerror
-         }
-
-
-
-# TESTING DATA
-test = {'single': ,
-        'ensemble': ,}
-
-
-
-# DATA DICTIONARY
-data = {'train': 0,
-        'valid': 0,
-        'test': 0}
-
-
-
+data = aggregateResults(res, eres)
 
 
 app = dash.Dash()
 
-options = ['Single Model', 'Ensemble Model'] + [str(k) for k in eres.train_accy.keys()]
-x_axis_options = ['Iterations', 'Epochs']
-dataset_options = ['Loss', 'Accuracy', 'Test Error']
+#options = ['Single Model', 'Ensemble Model'] + [str(k) for k in eres.train_accy.keys()]
+resolutions = [{'label': 'Iteration', 'value': 'iter'},
+               {'label': 'Epochs', 'value': 'epoch'}]
+
+measurements = [{'label': 'Loss', 'value': 'loss'},
+                {'label': 'Accuracy', 'value': 'accy'},
+                {'label': 'Test Error', 'value': 'accy'}]
+
 
 app.layout = html.Div([
         
-        dcc.Dropdown(id='measure-picker',
-                     options = dataset_options,
-                     value = dataset_options[1]),
+        # Training results
+        html.Div([
+
+                html.H1('Training Results'),
+                
+                dcc.Dropdown(id='measure-picker',
+                     options = measurements,
+                     value = measurements[1]),
     
-        dcc.Graph(id='graph'),
-        
-        dcc.Dropdown(id='season-picker',
-                     options = x_axis_options,
-                     value = x_axis_options[1])])
-    
-    
+                dcc.Graph(id='graph'),
+                
+                dcc.Dropdown(id='resolution-picker',
+                             options = resolutions,
+                             value = resolutions[1])
+                ])
+        ])
+
+
 @app.callback(Output(component_id = 'graph', component_property='figure'),
               [Input(component_id = 'measure-picker', component_property='value'),
                Input(component_id = 'resolution-picker', component_property='value')])    
-def update_graph(selected_season):
+def train_graph(measure, resolution):
+        
+    mlab = measure['label']
+    mval = measure['value']
     
-    
-    ## TODO: Conversor pickers to dict keys
-    
-    
+    rlab = resolution['laeb']
+    rval = resolution['value']
     
     # Return data only for the selection from the dropdown
-    filtered_df = class_global[class_global['season'] == selected_season]
+    df = data['train'][rval][mval]
+
     
     traces = []
     
-    
-    
-    for team in filtered_df['team_name'].unique():
-        df_by_team = filtered_df[filtered_df['team_name'] == team]
-        traces.append(go.Scatter(x = df_by_team['round'], 
-                                 y = df_by_team['position'],
-                                 name = team,
-                                 mode = 'lines+markers',
-                                 #hoverinfo = ['name' + '. ' + 'Jornada: ' + 'x' + ', Posición: ' + 'y'],
-                                 opacity = 0.7,
-                                 marker = {'size': 5},
-                                 visible = True if team in ['Real Madrid', 'Barcelona'] else 'legendonly'))
+    for col in df:
         
-    layout = go.Layout(title='Clasificación',
-                       xaxis = {'title': 'Jornadas'},
-                       yaxis = {'title': 'Posicion', 
-                                'autorange': 'reversed',
-                                'range': [0, 20],
-                                'dtick': 1,},
+        traces.append(
+                go.Scatter(
+                        #x = np.range(len(list(df))), 
+                        x = df.index.values+1, 
+                        y = df[col],
+                        name = col,
+                        mode = 'lines+markers',
+                        marker = {'size': 5},
+                        visible = True if col in ['ensemble', 'ResNet56'] else 'legendonly'
+                        )
+                )
+        
+    layout = go.Layout(title='Training Results',
+                       xaxis = {'title': rlab},
+                       yaxis = {'title': mlab, 
+                                'range': [0, 100],
+                                'dtick': 10,},
                        hovermode = 'closest')
     
     return {'data': traces, 'layout': layout}
     
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
