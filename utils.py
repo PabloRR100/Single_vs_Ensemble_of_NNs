d855a4c729e29ff5a5862a79a50be61e03bbacab
PabloRR100 <pabloruizruiz10@gmail.com>
2018-11-09 09:52:21 -0500

    timeit

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@
 
 import os
 import sys
+import time
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -37,6 +38,22 @@ errors = {
                 'batch_size':'n_iters and n_epochs must be divisible to compute the batch size'
                 }
         }
+        
+        
+# Decorator to time function executions
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            print('%r  %2.2f ms' % (method.__name__, (te - ts) * 1000))
+        return result
+    return timed
+        
         
 # Count parameters of a model 
 def count_parameters(model):
